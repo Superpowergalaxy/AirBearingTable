244bfc5446ab5d7e7b7adfb4c69dc303dfa63835
Jingyu Su <superpowergalaxy@gmail.com>
2017-09-13 12:49:32 -0400

    try10

--- motor_control_RR.py
+++ motor_control_RR.py
@@ -13,7 +13,7 @@ service experimental.minimal_create
 
 object create_obj
     #function void DriveSpeed(int16 velocity1, int16 velocity2,int16 velocity3)
-    function void SpeedUp(int16 velocity11, int16 velocity12,int16 velocity13):
+    function void SpeedUp(int16 velocity11, int16 velocity12,int16 velocity13)
 end object
 """
 
